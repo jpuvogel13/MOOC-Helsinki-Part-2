1c68c7ccddd3f1e8b1dcfc398740efabf2a60212
jpuvogel13 <31817732+jpuvogel13@users.noreply.github.com>
2019-10-15 23:50:25 +0800

    Add files via upload

---
+++ week9-week9_17.FirstPackages/src/mooc/ui/UserInterface.java
@@ -0,0 +1,6 @@
+package src.mooc.ui;
+
+public interface UserInterface {
+
+    void update();
+}
---
+++ week9-week9_17.FirstPackages/src/mooc/ui/logic/ApplicationLogic.java
@@ -0,0 +1,20 @@
+package src.mooc.ui.logic;
+
+import src.mooc.ui.UserInterface;
+
+public class ApplicationLogic {
+
+    private UserInterface ui;
+
+    public ApplicationLogic(UserInterface ui){
+        this.ui = ui;
+    }
+
+    public void execute(int howManyTimes){
+        for(int i=0; i<=howManyTimes; i++){
+            System.out.println("The application logic works");
+            ui.update();
+        }
+
+    }
+}
